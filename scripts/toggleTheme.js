8958f23f483066d30044372635b045f6cf7410f4
Harshit Gupta <hgtgt07@gmail.com>
2024-12-01 02:08:26 +0530

    blob with dark theme + profile-pic change

--- scripts/toggleTheme.js
+++ scripts/toggleTheme.js
@@ -47,6 +47,10 @@ function applyDarkMode() {
   document.documentElement.style.setProperty("--color-hg-light2", "#720455");
   document.documentElement.style.setProperty("--color-hg-dark2", "#3C0753");
   document.documentElement.style.setProperty("--boxShadow-red", "#85005b");
+  document.documentElement.style.setProperty(
+    "--blob-img",
+    'url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22b%22%3E%3Cpath fill=%22currentColor%22 d=%22M848 692.5q-41 192.5-236.5 183T320 749.5q-96-116.5-53.5-229t85-298q42.5-185.5 220-110T819 344q70 156 29 348.5Z%22%2F%3E%3C%2FclipPath%3E%3Cpattern id=%22a%22 patternUnits=%22userSpaceOnUse%22 width=%2225%22 height=%2225%22 viewBox=%220 0 100 100%22 fill=%22%2385005b%22%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2212.5%22%2F%3E%3C%2Fpattern%3E%3C%2Fdefs%3E%3Cpath fill=%22url(%23a)%22 d=%22M848 692.5q-41 192.5-236.5 183T320 749.5q-96-116.5-53.5-229t85-298q42.5-185.5 220-110T819 344q70 156 29 348.5Z%22 transform=%22translate(-60 -60)%22%2F%3E%3Cg clip-path=%22url(%23b)%22%3E%3Cpath fill=%22%2385005b%22 d=%22M848 692.5q-41 192.5-236.5 183T320 749.5q-96-116.5-53.5-229t85-298q42.5-185.5 220-110T819 344q70 156 29 348.5Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")'
+  );
   document.querySelectorAll(".wrapper a").forEach((a) => {
     a.style.color = "#fff";
     a.style.borderColor = "#fff";
@@ -73,6 +77,10 @@ function applyLightMode() {
   document.documentElement.style.setProperty("--color-hg-light2", "#ff4444");
   document.documentElement.style.setProperty("--color-hg-dark2", "#eee");
   document.documentElement.style.setProperty("--boxShadow-red", "#c77");
+  document.documentElement.style.setProperty(
+    "--blob-img",
+    'url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22b%22%3E%3Cpath fill=%22currentColor%22 d=%22M848 692.5q-41 192.5-236.5 183T320 749.5q-96-116.5-53.5-229t85-298q42.5-185.5 220-110T819 344q70 156 29 348.5Z%22%2F%3E%3C%2FclipPath%3E%3Cpattern id=%22a%22 patternUnits=%22userSpaceOnUse%22 width=%2225%22 height=%2225%22 viewBox=%220 0 100 100%22 fill=%22%23ff4e4e%22%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2212.5%22%2F%3E%3C%2Fpattern%3E%3C%2Fdefs%3E%3Cpath fill=%22url(%23a)%22 d=%22M848 692.5q-41 192.5-236.5 183T320 749.5q-96-116.5-53.5-229t85-298q42.5-185.5 220-110T819 344q70 156 29 348.5Z%22 transform=%22translate(-60 -60)%22%2F%3E%3Cg clip-path=%22url(%23b)%22%3E%3Cpath fill=%22%23ff4e4e%22 d=%22M848 692.5q-41 192.5-236.5 183T320 749.5q-96-116.5-53.5-229t85-298q42.5-185.5 220-110T819 344q70 156 29 348.5Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")'
+  );
   document.querySelectorAll(".wrapper a").forEach((a) => {
     a.style.cssText = "";
   });
